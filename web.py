a6f959af24a7ebd9c4ec260b0e6c46768b90cc80
Nyalia Lui <luinyalia@gmail.com>
2019-11-10 18:21:18 -0500

    Finished visualizations
    Prototype complete. Now we just need blizz to update s2protocol so I can use this realtime. I guess in meantime we will work on getting webservice setup on AWS and converting flaskapp to expressjs.

--- web.py
+++ web.py
@@ -9,47 +9,71 @@ def setup_app():
 
 @app.route("/")
 def home():
-    matches = db.mhistory.find()
+    temp = db.mhistory.find()
+    record = {"ratio": [0, 0]}
+    matches = []
 
-    return render_template("match_history.html", matches=matches)
+    for match in temp:
+        if match["player"]["win"]:
+            record["ratio"][0] += 1
+        else:
+            record["ratio"][1] += 1
+        
+        matches.append(match)
 
-@app.route("/mhistory")
-def page_mhistory():
-    matches = db.mhistory.find()
-
-    return render_template("match_history.html", matches=matches)
+    return render_template("match_history.html", matches=matches, record=record)
 
 @app.route("/mhistory/pname/<name>")
 def page_player(name):
     temp = name.split('-')
     clan = temp[0]
     pname = temp[1]
-    matches = db.mhistory.find({"opponent.name": pname, "opponent.clan_tag": clan})
+    temp = db.mhistory.find({"opponent.name": pname, "opponent.clan_tag": clan})
+    record = {"ratio": [0, 0]}
+    matches = []
 
-    return render_template("player.html", pname=pname, clan_tag=clan, matches=matches)
+    for match in temp:
+        if match["player"]["win"]:
+            record["ratio"][0] += 1
+        else:
+            record["ratio"][1] += 1
+        
+        matches.append(match)
+
+    return render_template("player.html", pname=pname, clan_tag=clan, matches=matches, record=record)
 
 @app.route("/mhistory/map/<mapcode>")
 def page_map(mapcode):
     matches = db.mhistory.find({"mcode": mapcode})
     name = matches[0]["map"]
     record = {}
-    record["Zerg"] = {"win": 0, "loss": 0}
-    record["Terran"] = {"win": 0, "loss": 0}
-    record["Protoss"] = {"win": 0, "loss": 0}
+    record["Zerg"] = [0, 0]
+    record["Terran"] = [0, 0]
+    record["Protoss"] = [0, 0]
 
     for match in matches:
         if match["player"]["win"]:
-            record[match["opponent"]["race"]]["win"] += 1
+            record[match["opponent"]["race"]][0] += 1
         else:
-            record[match["opponent"]["race"]]["loss"] += 1
+            record[match["opponent"]["race"]][1] += 1
 
     return render_template("map.html", mname=name, record=record)
 
 @app.route("/mhistory/date/<UTC>")
 def page_date(UTC):
-    matches = db.mhistory.find({"date": UTC})
+    temp = db.mhistory.find({"date": UTC})
+    record = {"ratio": [0, 0]}
+    matches = []
 
-    return render_template("date.html", date=UTC, matches=matches)
+    for match in temp:
+        if match["player"]["win"]:
+            record["ratio"][0] += 1
+        else:
+            record["ratio"][1] += 1
+        
+        matches.append(match)
+
+    return render_template("date.html", date=UTC, matches=matches, record=record)
 
 @app.route("/signin")
 def page_signin():
